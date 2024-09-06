05fbd622953030c3675557aa60e9d542ca830317
Mustafa Mehmedoglu <mikbalm@gmail.com>
2024-09-05 23:16:06 -0400

    passes all tests!

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+
+    @property
+    def brand(self):
+        return self._brand
+
+    @brand.setter
+    def brand(self, brand):
+        if type(brand) is not str:
+            print("brand must be a string")
+        else:
+            self._brand = brand
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, size):
+        if type(size) is not int:
+            print("size must be an integer")
+        else:
+            self._size = size
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
